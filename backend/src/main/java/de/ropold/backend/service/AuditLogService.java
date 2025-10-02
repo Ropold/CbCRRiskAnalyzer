7a5d01f9fcb69048e54781f84381d0852535a370
Robert Stolz <robert.stolz87@gmail.com>
2025-10-02 10:56:40 +0200

    AuditLog setup

---
+++ backend/src/main/java/de/ropold/backend/service/AuditLogService.java
@@ -0,0 +1,12 @@
+package de.ropold.backend.service;
+
+import de.ropold.backend.repository.AuditLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuditLogService {
+
+    private final AuditLogRepository auditLogRepository;
+}
